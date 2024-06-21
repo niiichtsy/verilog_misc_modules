e9d0e946ad170f7346a001b635cbd069de29819e
bojkem <bojkem@transbit.com.pl>
2024-06-21 14:43:48 +0200

    Re-done clk_stabilizer to function properly

--- utils/clk_stabilizer/src/clk_stabilizer.v
+++ utils/clk_stabilizer/src/clk_stabilizer.v
@@ -1,24 +1,21 @@
-`timescale 1ns / 1ps
-
 module clk_stabilizer (
     input clk_select,
-    output reg out_clk,
-    input stable_clk,
     input prog_clk,
-    input resetn
+    input stable_clk,
+    output wire out_clk
 
 );
 
-  assign clk_out_s = out_clk;
-  assign clk_prog_s = prog_clk;
-  assign clk_stable_s = stable_clk;
+  assign out_clk = out_clk_s;
+  assign prog_clk_s = prog_clk;
+  assign stable_clk_s = stable_clk;
   assign clock_select_s = clk_select;
 
   BUFGMUX_CTRL i_clock_mux (
       .S (clock_select_s),
-      .I0(clk_prog_s),
-      .I1(clk_stable_s),
-      .O (clk_out_s)
+      .I0(prog_clk_s),
+      .I1(stable_clk_s),
+      .O (out_clk_s)
   );
 
   // Simulate waves
